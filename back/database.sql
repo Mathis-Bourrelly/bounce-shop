b1fac03b1db3e9b671a06a1a22e274a09211c6a4
Mathis Bourrelly <mathis.bourrelly13@gmail.com>
2024-07-02 16:10:22 +0200

    edit bdd config

--- back/database.sql
+++ back/database.sql
@@ -1,23 +1,3 @@
-DO
-$$
-    BEGIN
-        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'BOUNCEUSER') THEN
-            CREATE USER "BOUNCEUSER" WITH PASSWORD 'BounceShop!13';
-        END IF;
-    END
-$$;
-
-DO
-$$
-    BEGIN
-        IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'BOUNCESHOP') THEN
-            CREATE DATABASE "BOUNCESHOP";
-        END IF;
-    END
-$$;
-
-GRANT ALL PRIVILEGES ON DATABASE "BOUNCESHOP" TO "BOUNCEUSER";
-
 drop table "Prices" cascade;
 
 drop table "PreviousParts" cascade;
