7e67aaacfceec005d3468f785c2840e675e78522
Mathis Bourrelly <mathis.bourrelly13@gmail.com>
2024-07-03 14:37:57 +0200

    fix ssl

--- back/src/core/web-server.js
+++ back/src/core/web-server.js
@@ -22,7 +22,9 @@ class WebServer {
     options = {
         key: readFileSync(join(__dirname, "../../key.pem")),
         cert: readFileSync(join(__dirname, "../../cert.pem")),
+        ca: readFileSync(join(__dirname, "../../client.csr")),
         passphrase: 'bounceshop'
+
     };
 
     constructor() {
